9d9c46ca79fb970b1769e4fc05ab74f4ff006aad
AnanyVerma01 <vermaanany789@gmail.com>
2025-07-18 16:46:26 +0530

    Time: 493 ms (47.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT w2.id AS Id
+FROM Weather w1
+INNER JOIN Weather w2
+WHERE DATEDIFF(w2.recordDate,w1.recordDate)=1
+AND w2.temperature > w1.temperature
+GROUP BY w2.id
