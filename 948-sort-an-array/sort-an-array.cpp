1deded9e3c1ea0770f86eca07ba1c55d6aa49553
AnanyVerma01 <vermaanany789@gmail.com>
2025-10-23 11:38:55 +0530

    Time: 98 ms (61.72%) | Memory: 99 MB (50.91%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,50 +1,27 @@
 class Solution {
+private:
+    void countingSort(vector<int> &arr) {
+        unordered_map<int, int> count;
+        int minVal = *min_element(arr.begin(), arr.end());
+        int maxVal = *max_element(arr.begin(), arr.end());
+
+        for (auto& val : arr) {
+            count[val]++;
+        }
+
+        int index = 0;
+        for (int val = minVal; val <= maxVal; ++val) {
+            while (count[val] > 0) {
+                arr[index] = val;
+                index++;
+                count[val]--;
+            }
+        }
+    }
+
 public:
-    vector<int> sortArray(vector<int>& nums) 
-    {
-        mergeSort(nums,0,nums.size()-1);
+    vector<int> sortArray(vector<int>& nums) {
+        countingSort(nums);
         return nums;
-    }
-
-    void mergeSort(vector<int>& nums, int start, int end)
-    {
-        if(start==end) return;
-        int mid = start + (end-start)/2;
-        mergeSort(nums,start,mid);
-        mergeSort(nums,mid+1,end);
-        merge(nums,start,mid,end);
-    }
-
-    void merge(vector<int>& nums, int start, int mid, int end)
-    {
-        vector<int> temp(end-start+1);
-        int left = start, right = mid+1, idx = 0;
-
-        while(left<=mid && right<=end){
-            if(nums[left]<nums[right]){
-                temp[idx]=nums[left];
-                left++; idx++;
-            }
-            else{
-                temp[idx]=nums[right];
-                right++; idx++;
-            }
-        }
-
-        while(left<=mid){
-            temp[idx]=nums[left];
-            left++; idx++;
-        }
-
-        while(right<=end){
-            temp[idx]=nums[right];
-            right++; idx++;
-        }
-
-        idx=0;
-
-        while(start<=end){
-            nums[start++]=temp[idx++];
-        }
     }
 };
