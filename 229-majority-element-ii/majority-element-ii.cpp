3cb01b4f22e0a2b1e4346cea74d3a4376b7efb1c
AnanyVerma01 <vermaanany789@gmail.com>
2025-10-24 16:45:19 +0530

    Time: 8 ms (31.81%) | Memory: 26.4 MB (17.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -9,13 +9,9 @@ public:
             map[n]=1+map[n];
         }
         
-        for(int n : nums){
-            if(map[n]>ceil(s/3)){
-                auto it = find(v.begin(), v.end(), n);
-                if(it == v.end()){
-                    v.push_back(n);
-                }
-
+        for(auto n : map){
+            if(n.second > (s/3)){
+                v.push_back(n.first);
             }
         }
 
