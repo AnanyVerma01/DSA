687befd60194f639670a1766ef252b783027a4ee
AnanyVerma01 <vermaanany789@gmail.com>
2025-10-24 13:38:09 +0530

    Time: 6 ms (38.36%) | Memory: 22.1 MB (63.47%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool validPalindrome(string s) {
+        int left=0,right=s.length()-1;
+
+        while(left<right){
+            if(s[left]!=s[right]){
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+            }
+                left++;
+                right--;
+        }
+
+        return true;
+    }
+
+    bool isPalindrome(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+};
