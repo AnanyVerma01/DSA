42148d345d4681f0e40eaa6cf9dcb09e23166257
AnanyVerma01 <vermaanany789@gmail.com>
2025-11-07 06:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (66.48%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -12,40 +12,34 @@ public:
     }
     
     int pop() {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
+        if(s2.empty())
+        {
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
-
         int top = s2.top();
         s2.pop();
-
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
 
         return top;
     }
     
     int peek() {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
+        if(s2.empty())
+        {
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
-        
         int top = s2.top();
-
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
 
         return top;
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
