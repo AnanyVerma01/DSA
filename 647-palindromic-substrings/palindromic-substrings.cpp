769c6ed001e80ca6d6b90e3b133ad0d2652ee4c1
AnanyVerma01 <vermaanany789@gmail.com>
2025-11-28 05:13:35 +0530

    Time: 1401 ms (6.19%) | Memory: 406.7 MB (9.38%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPal(string s, int i, int j)
+    {
+        if(i==j) return true;
+
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++; j--;
+        }
+
+        return true;
+    }
+    
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPal(s,i,j)){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
