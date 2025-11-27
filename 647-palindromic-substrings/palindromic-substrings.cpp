f6a264258ef80154b666b255530d780b58c105bc
AnanyVerma01 <vermaanany789@gmail.com>
2025-11-28 05:16:33 +0530

    Time: 3 ms (88.34%) | Memory: 8.5 MB (79.88%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool isPal(string s, int i, int j)
-    {
-        if(i==j) return true;
-
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++; j--;
-        }
-
-        return true;
-    }
-    
     int countSubstrings(string s) {
-        int n = s.size();
-        int count=0;
+        int res = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPal(s,i,j)){
-                    count++;
-                }
+        for (int i = 0; i < s.size(); i++) {
+            // odd length
+            int l = i, r = i;
+            while (l >= 0 && r < s.size() &&
+                   s[l] == s[r]) {
+                res++;
+                l--;
+                r++;
+            }
+
+            // even length
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.size() &&
+                   s[l] == s[r]) {
+                res++;
+                l--;
+                r++;
             }
         }
 
-        return count;
+        return res;
     }
 };
