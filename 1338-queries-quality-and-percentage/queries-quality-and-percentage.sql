c7243448e603aab0807a97c1718ac089cc6fba5d
AnanyVerma01 <vermaanany789@gmail.com>
2026-01-17 21:47:20 +0530

    Time: 361 ms (34.54%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,5 +1,5 @@
-SELECT query_name, ROUND(AVG(q.rating/q.position),2) AS quality,
-ROUND(AVG(if(q.rating<3, 1,0)*100),2) AS poor_query_percentage
-FROM Queries q
-GROUP BY q.query_name
-
+select query_name, round(SUM(rating/position)/count(*),2) as quality,
+Round(sum(if(rating<3,1,0))/count(*)*100,2) as poor_query_percentage
+from Queries 
+group by query_name
+order by poor_query_percentage
