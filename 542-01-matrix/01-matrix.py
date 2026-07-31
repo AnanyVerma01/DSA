9ac82aa61c1a1aaed283698dac7d6d0b8caa9d38
AnanyVerma01 <vermaanany789@gmail.com>
2026-08-01 01:17:29 +0530

    Time: 255 ms (18.04%) | Memory: 17.8 MB (7.19%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -2,28 +2,28 @@ class Solution(object):
     def updateMatrix(self, mat):
         rows = len(mat)
         cols = len(mat[0])
-
-        visited = [[0 for i in range(cols)] for _ in range(rows)]
+        visited = [[0 for _ in range(cols)] for _ in range(rows)]
         distance = deepcopy(visited)
-
         queue = deque()
+
         for i in range(rows):
             for j in range(cols):
                 if mat[i][j]==0:
                     queue.append((i,j,0))
                     visited[i][j]=1
-        
-        while len(queue)!=0:
-            di,dj,dis = queue.popleft()
-            distance[di][dj]=dis
 
-            for i,j in [(-1,0),(1,0),(0,-1),(0,1)]:
-                newi,newj=di+i,dj+j
-                if newi<0 or newi==rows or newj<0 or newj==cols:
+        while len(queue)!=0:
+            i,j,dis = queue.popleft()
+            distance[i][j]=dis
+            for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                newi,newj=i+dx,j+dy
+                if newi<0 or newj<0 or newj>=cols or newi>=rows:
                     continue
                 if visited[newi][newj]==1:
                     continue
                 visited[newi][newj]=1
                 queue.append((newi,newj,dis+1))
+                    
 
         return distance
+        
