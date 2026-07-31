27711b702a51b45e02b35e26094b9242426ab82b
AnanyVerma01 <vermaanany789@gmail.com>
2026-08-01 00:29:44 +0530

    Time: 175 ms (22.11%) | Memory: 74.2 MB (49.87%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,60 +1,33 @@
 class Solution(object):
     def numEnclaves(self, grid):
-        rows=len(grid)
-        cols=len(grid[0])
-        visited=[[0 for _ in range(cols)] for _ in range(rows)]
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = [[0 for _ in range(cols)] for _ in range(rows)]
 
-        queue = deque()
+        def recur(i,j,board,visited):
+            if i<0 or j<0 or i>=len(board) or j>=len(board[0]):
+                return
+            if board[i][j]==0 or visited[i][j]==1:
+                return
+
+            visited[i][j]=1
+            
+            recur(i-1,j,board,visited)
+            recur(i+1,j,board,visited)
+            recur(i,j-1,board,visited)
+            recur(i,j+1,board,visited)
+
         for i in range(rows):
             for j in range(cols):
-                if i==0 or j==0 or i==rows-1 or j==cols-1:
-                    if grid[i][j]==1:
-                        queue.append((i,j))
-
-        while len(queue)!=0:
-            i,j = queue.pop()
-            visited[i][j]=1
-            for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                newi,newj=i+dx,j+dy
-                if newi<0 or newj<0 or newi==rows or newj==cols:
-                    continue
-                if grid[newi][newj]==0 or visited[newi][newj]==1:
-                    continue
-                visited[newi][newj]=1
-                queue.append((newi,newj))
-
+                if i==0 or i==rows-1 or j==0 or j==cols-1:
+                    if grid[i][j]==1 and visited[i][j]==0:
+                        recur(i,j,grid,visited)
+        
         count=0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j]==1 and visited[i][j]==0:
+                if grid[i][j]==1 and not visited[i][j]:
                     count+=1
 
-        return count 
-        
-        # def dfs(i,j,rows,cols,grid,visited):
-        #     if i<0 or j<0 or i>=rows or j>=cols:
-        #         return
-        #     if grid[i][j]==0 or visited[i][j]==1:
-        #         return
-        #     visited[i][j]=1
-        #     dfs(i-1,j,rows,cols,grid,visited)
-        #     dfs(i+1,j,rows,cols,grid,visited)
-        #     dfs(i,j-1,rows,cols,grid,visited)
-        #     dfs(i,j+1,rows,cols,grid,visited)
-
-        
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if i==0 or j==0 or i==rows-1 or j==cols-1:
-        #             if grid[i][j]==1:
-        #                 dfs(i,j,rows,cols,grid,visited)
-
-        # count=0
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if visited[i][j]==0 and grid[i][j]==1:
-        #             count+=1
-
-        # return count
-                    
+        return count
         
