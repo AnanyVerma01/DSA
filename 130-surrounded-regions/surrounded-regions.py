3e362bf4c10e0b5d1a226983ddc3093eaeb1131f
AnanyVerma01 <vermaanany789@gmail.com>
2026-08-01 00:24:30 +0530

    Time: 14 ms (17.24%) | Memory: 16.6 MB (74.26%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,30 +1,29 @@
 class Solution(object):
     def solve(self, board):
-        rows=len(board)
-        cols=len(board[0])
-        visited = [[0 for i in range(cols)] for _ in range(rows)]
+        rows = len(board)
+        cols = len(board[0])
+        visited = [[0 for _ in range(cols)] for _ in range(rows)]
 
-        def dfs(i,j,rows,cols,board,visited):
-            if i<0 or j<0 or i>=rows or j>=cols:
+        def recur(i,j,board,visited):
+            if i<0 or j<0 or i>=len(board) or j>=len(board[0]):
                 return
             if board[i][j]=="X" or visited[i][j]==1:
                 return
-            visited[i][j]=1
-            dfs(i-1,j,rows,cols,board,visited)
-            dfs(i+1,j,rows,cols,board,visited)
-            dfs(i,j-1,rows,cols,board,visited)
-            dfs(i,j+1,rows,cols,board,visited)
 
+            visited[i][j]=1
+            
+            recur(i-1,j,board,visited)
+            recur(i+1,j,board,visited)
+            recur(i,j-1,board,visited)
+            recur(i,j+1,board,visited)
+
+        for i in range(rows):
+            for j in range(cols):
+                if i==0 or i==rows-1 or j==0 or j==cols-1:
+                    if board[i][j]=="O" and visited[i][j]==0:
+                        recur(i,j,board,visited)
         
         for i in range(rows):
             for j in range(cols):
-                if i==0 or j==0 or i==rows-1 or j==cols-1:
-                    if board[i][j]=='O':
-                        dfs(i,j,rows,cols,board,visited)
-                
-                    
-        
-        for i in range(rows):
-            for j in range(cols):
-                if visited[i][j]==0:
+                if board[i][j]=="O" and not visited[i][j]:
                     board[i][j]="X"
