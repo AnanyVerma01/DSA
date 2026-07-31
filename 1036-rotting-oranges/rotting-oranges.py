8699c264b0708d002744d7109a98b05cd16360fe
AnanyVerma01 <vermaanany789@gmail.com>
2026-07-31 23:42:16 +0530

    Time: 12 ms (7.93%) | Memory: 12.4 MB (23.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,35 +2,39 @@ class Solution(object):
     def orangesRotting(self, grid):
         rows = len(grid)
         cols = len(grid[0])
-        mat = deepcopy(grid)
+        matrix = deepcopy(grid)
+
         queue = deque()
         fresh = 0
-
-        for i in range(0,rows):
-            for j in range(0,cols):
-                if mat[i][j] == 2:
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==1:
+                    fresh+=1
+                if grid[i][j]==2:
                     queue.append((i,j))
-                if mat[i][j] == 1:
-                    fresh = 1+fresh
 
-        minutes = 0
-        while len(queue)!=0 and fresh>0:
-            minutes = minutes+1
-            times = len(queue)
+        if fresh==0:
+            return 0
 
-            for _ in range(times):
+        seconds=0
+        while len(queue)!=0:
+            size = len(queue)
+            seconds+=1
+            for times in range(size):
                 i,j = queue.popleft()
                 for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                    if i+dx<0 or i+dx==rows or j+dy<0 or j+dy==cols:
+                    newi,newj=i+dx,j+dy
+                    if newi<0 or newj<0 or newi>=rows or newj>=cols:
                         continue
-                    if mat[i+dx][j+dy]==0 or mat[i+dx][j+dy]==2:
+                    if matrix[newi][newj]==2 or matrix[newi][newj]==0:
                         continue
-
-                    fresh=fresh-1
-                    mat[i+dx][j+dy]=2
-                    queue.append((i+dx,j+dy))
-
-
-        if fresh>0:
+                    fresh-=1
+                    queue.append((newi,newj))
+                    matrix[newi][newj]=2
+        
+        if fresh!=0:
             return -1
-        return minutes
+        else: 
+            return seconds-1
+
+        
