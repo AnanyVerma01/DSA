f1ed3ffe9ac4dda8cc34566d20b48ee83b917325
AnanyVerma01 <vermaanany789@gmail.com>
2026-08-07 22:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.77%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        idx=0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[idx]=nums[i]
+                idx+=1
+
+        return idx
